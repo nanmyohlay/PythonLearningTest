417dbf2103af74f088692bf6e74597ee3dc9dbc1
nanmyohlay <65281726+nanmyohlay@users.noreply.github.com>
2020-06-19 03:27:39 -0700

    feat: Section 3(Lists)

---
+++ List Test.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+myList=['one', 'two', 'three']
+myList
+
+
+# In[5]:
+
+
+my_list = [2,4,6,8]
+my_list
+
+
+# In[6]:
+
+
+my_list[2]
+
+
+# In[7]:
+
+
+len(my_list)
+
+
+# In[8]:
+
+
+myList.append('next')
+myList
+
+
+# In[16]:
+
+
+my_list.append(7)
+my_list
+
+
+# In[10]:
+
+
+myList.sort()
+
+
+# In[11]:
+
+
+myList
+
+
+# In[ ]:
+
+
+my_list.sort()
+
+
+# In[17]:
+
+
+my_list
+
+
+# In[18]:
+
+
+my_next=[6,2,7,9,10]
+my_next
+
+
+# In[19]:
+
+
+my_next.append(1)
+
+
+# In[20]:
+
+
+my_next
+
+
+# In[21]:
+
+
+my_next.sort()
+my_next
+
+
+# In[22]:
+
+
+len(my_next)
+
+
+# In[23]:
+
+
+my_next.reverse()
+my_next
+
+
+# In[24]:
+
+
+my_next[1:]
+
+
+# In[25]:
+
+
+my_next[:3]
+
+
+# In[26]:
+
+
+my_next[0]='Hla Hla'
+
+
+# In[27]:
+
+
+my_next
+
+
+# In[ ]:
+
+
+
+
